2897bf1089bbaa273e982f901c85d6a2a7940a83
baobingwen <23009200067@stu.xidian.edu.cn>
2025-05-31 14:59:22 +0800

    Site updated: 2025-05-31 14:59:17

---
+++ games/MineSweeper/core.js
@@ -0,0 +1,252 @@
+let wasm;
+
+let cachedUint8ArrayMemory0 = null;
+
+function getUint8ArrayMemory0() {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
+        cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8ArrayMemory0;
+}
+
+function getArrayU8FromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return getUint8ArrayMemory0().subarray(ptr / 1, ptr / 1 + len);
+}
+
+function addToExternrefTable0(obj) {
+    const idx = wasm.__externref_table_alloc();
+    wasm.__wbindgen_export_2.set(idx, obj);
+    return idx;
+}
+
+function handleError(f, args) {
+    try {
+        return f.apply(this, args);
+    } catch (e) {
+        const idx = addToExternrefTable0(e);
+        wasm.__wbindgen_exn_store(idx);
+    }
+}
+
+const cachedTextDecoder = (typeof TextDecoder !== 'undefined' ? new TextDecoder('utf-8', { ignoreBOM: true, fatal: true }) : { decode: () => { throw Error('TextDecoder not available') } } );
+
+if (typeof TextDecoder !== 'undefined') { cachedTextDecoder.decode(); };
+
+function getStringFromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return cachedTextDecoder.decode(getUint8ArrayMemory0().subarray(ptr, ptr + len));
+}
+
+function takeFromExternrefTable0(idx) {
+    const value = wasm.__wbindgen_export_2.get(idx);
+    wasm.__externref_table_dealloc(idx);
+    return value;
+}
+
+function isLikeNone(x) {
+    return x === undefined || x === null;
+}
+
+const MineSweeperCoreWasmFinalization = (typeof FinalizationRegistry === 'undefined')
+    ? { register: () => {}, unregister: () => {} }
+    : new FinalizationRegistry(ptr => wasm.__wbg_minesweepercorewasm_free(ptr >>> 0, 1));
+
+export class MineSweeperCoreWasm {
+
+    __destroy_into_raw() {
+        const ptr = this.__wbg_ptr;
+        this.__wbg_ptr = 0;
+        MineSweeperCoreWasmFinalization.unregister(this);
+        return ptr;
+    }
+
+    free() {
+        const ptr = this.__destroy_into_raw();
+        wasm.__wbg_minesweepercorewasm_free(ptr, 0);
+    }
+    /**
+     * @param {number} rows
+     * @param {number} cols
+     * @param {number} mines
+     */
+    constructor(rows, cols, mines) {
+        const ret = wasm.minesweepercorewasm_new(rows, cols, mines);
+        if (ret[2]) {
+            throw takeFromExternrefTable0(ret[1]);
+        }
+        this.__wbg_ptr = ret[0] >>> 0;
+        MineSweeperCoreWasmFinalization.register(this, this.__wbg_ptr, this);
+        return this;
+    }
+    /**
+     * @param {bigint | null} [seed]
+     */
+    generate_mines(seed) {
+        wasm.minesweepercorewasm_generate_mines(this.__wbg_ptr, !isLikeNone(seed), isLikeNone(seed) ? BigInt(0) : seed);
+    }
+    reset() {
+        wasm.minesweepercorewasm_reset(this.__wbg_ptr);
+    }
+    /**
+     * @param {number} row
+     * @param {number} col
+     */
+    openCell(row, col) {
+        wasm.minesweepercorewasm_openCell(this.__wbg_ptr, row, col);
+    }
+    /**
+     * @returns {Array<any>}
+     */
+    getVisibleBoard() {
+        const ret = wasm.minesweepercorewasm_getVisibleBoard(this.__wbg_ptr);
+        return ret;
+    }
+    /**
+     * @returns {number}
+     */
+    getGameState() {
+        const ret = wasm.minesweepercorewasm_getGameState(this.__wbg_ptr);
+        return ret;
+    }
+}
+
+async function __wbg_load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+
+            } catch (e) {
+                if (module.headers.get('Content-Type') != 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve Wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+
+        } else {
+            return instance;
+        }
+    }
+}
+
+function __wbg_get_imports() {
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbg_getRandomValues_3c9c0d586e575a16 = function() { return handleError(function (arg0, arg1) {
+        globalThis.crypto.getRandomValues(getArrayU8FromWasm0(arg0, arg1));
+    }, arguments) };
+    imports.wbg.__wbg_new_78feb108b6472713 = function() {
+        const ret = new Array();
+        return ret;
+    };
+    imports.wbg.__wbg_push_737cfc8c1432c2c6 = function(arg0, arg1) {
+        const ret = arg0.push(arg1);
+        return ret;
+    };
+    imports.wbg.__wbindgen_init_externref_table = function() {
+        const table = wasm.__wbindgen_export_2;
+        const offset = table.grow(4);
+        table.set(0, undefined);
+        table.set(offset + 0, undefined);
+        table.set(offset + 1, null);
+        table.set(offset + 2, true);
+        table.set(offset + 3, false);
+        ;
+    };
+    imports.wbg.__wbindgen_number_new = function(arg0) {
+        const ret = arg0;
+        return ret;
+    };
+    imports.wbg.__wbindgen_string_new = function(arg0, arg1) {
+        const ret = getStringFromWasm0(arg0, arg1);
+        return ret;
+    };
+    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+        throw new Error(getStringFromWasm0(arg0, arg1));
+    };
+
+    return imports;
+}
+
+function __wbg_init_memory(imports, memory) {
+
+}
+
+function __wbg_finalize_init(instance, module) {
+    wasm = instance.exports;
+    __wbg_init.__wbindgen_wasm_module = module;
+    cachedUint8ArrayMemory0 = null;
+
+
+    wasm.__wbindgen_start();
+    return wasm;
+}
+
+function initSync(module) {
+    if (wasm !== undefined) return wasm;
+
+
+    if (typeof module !== 'undefined') {
+        if (Object.getPrototypeOf(module) === Object.prototype) {
+            ({module} = module)
+        } else {
+            console.warn('using deprecated parameters for `initSync()`; pass a single object instead')
+        }
+    }
+
+    const imports = __wbg_get_imports();
+
+    __wbg_init_memory(imports);
+
+    if (!(module instanceof WebAssembly.Module)) {
+        module = new WebAssembly.Module(module);
+    }
+
+    const instance = new WebAssembly.Instance(module, imports);
+
+    return __wbg_finalize_init(instance, module);
+}
+
+async function __wbg_init(module_or_path) {
+    if (wasm !== undefined) return wasm;
+
+
+    if (typeof module_or_path !== 'undefined') {
+        if (Object.getPrototypeOf(module_or_path) === Object.prototype) {
+            ({module_or_path} = module_or_path)
+        } else {
+            console.warn('using deprecated parameters for the initialization function; pass a single object instead')
+        }
+    }
+
+    if (typeof module_or_path === 'undefined') {
+        module_or_path = new URL('core_bg.wasm', import.meta.url);
+    }
+    const imports = __wbg_get_imports();
+
+    if (typeof module_or_path === 'string' || (typeof Request === 'function' && module_or_path instanceof Request) || (typeof URL === 'function' && module_or_path instanceof URL)) {
+        module_or_path = fetch(module_or_path);
+    }
+
+    __wbg_init_memory(imports);
+
+    const { instance, module } = await __wbg_load(await module_or_path, imports);
+
+    return __wbg_finalize_init(instance, module);
+}
+
+export { initSync };
+export default __wbg_init;
--- tools/test/wasm_LP_test/worker.js
+++ tools/test/wasm_LP_test/worker.js
@@ -2,35 +2,45 @@
 import init, { Gilbert2D } from './wasm/lp_crypt_wasm_core.js';
 
 // 初始化WASM模块
-const wasmInitPromise =  init().then(() =>{
-        // console.log('Gilbert2D 类是否定义:', typeof Gilbert2D); // 应输出 "function"
-    })
-    .catch((error) => {
+const wasmInitPromise =  init().catch((error) => {
         console.error('Error initializing WASM module:', error);
         throw error;
     }
 );
 
 self.onmessage = async function (e) {
-    console.log('[Worker] Received Message:', e.data);
+    let gilbert = null;
+    try {
+        console.log('[Worker] Received Message:', e.data);
 
-    await wasmInitPromise; // 等待WASM模块初始化完成
+        await wasmInitPromise; // 等待WASM模块初始化完成
 
-    const { imgData, width, height, offset, isEncrypt } = e.data;
-    console.log('[Worker] Data:', imgData, width, height, offset, isEncrypt);
+        const { imgData, width, height, offset, isEncrypt } = e.data;
+        console.log('[Worker] Data:', imgData, width, height, offset, isEncrypt);
 
-    const gilbert = new Gilbert2D(width, height);
-    if (!gilbert) {
-        throw new Error("Gilbert2D 对象未正确初始化");
+        const gilbert = new Gilbert2D(width, height);
+        if (!gilbert) {
+            throw new Error("Gilbert2D 对象未正确初始化");
+        }
+
+        const buffer = new Uint8Array(imgData.data.buffer);
+        gilbert.process_pixels(buffer, offset, isEncrypt);
+
+        self.postMessage({
+            type: 'result',
+            buffer: buffer.buffer,
+        }, [buffer.buffer]); // 使用 Transferable
+
+        gilbert.free(); // 释放WASM内存
+    } catch (error) {
+        console.error('[Worker] Error:', error.message);
+        self.postMessage({
+            type: 'error',
+            message: `[Worker] 处理图像出错: ${error.message}`,
+        });
+    } finally {
+        if (gilbert !== null) {
+            gilbert.free(); // 确保释放gilbert内存
+        }
     }
-
-    const buffer = new Uint8Array(imgData.data.buffer);
-    gilbert.process_pixels(buffer, offset, isEncrypt);
-
-    self.postMessage({
-        type: 'result',
-        buffer: buffer.buffer,
-    }, [buffer.buffer]); // 使用 Transferable
-
-    gilbert.free(); // 释放WASM内存
 };
